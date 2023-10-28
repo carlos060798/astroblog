c8828dc4737ea38f2b5c0a440a4360a327cd6b8c
carlos danilo angarita <daniloangaritagarcia@gmail.com>
2023-10-28 11:52:15 -0500

    version 3

---
+++ src/scripts/menu.js
@@ -0,0 +1,5 @@
+// archivo de script para astro de la funcion menu
+
+document.querySelector('.hamburger').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('expanded');
+  });
